72d6fbe26005cca81d82b671a6be27c284bcc120
Michael Glaske <mglaske@gmail.com>
2022-08-22 19:32:16 +0000

    Packaging

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 Michael Glaske. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package dayofweek defines a representation of weekdays in a week
+package dayofweek
